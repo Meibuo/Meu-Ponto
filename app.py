d63ae2b4641cbb86500848677ed7d299bd1fb217
Meibuo <willfernandu@gmail.com>
2025-10-18 15:46:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -61,7 +61,6 @@ def dashboard():
     return "Bem-vindo ao Dashboard!"
 
 if __name__ == "__main__":
-    # Cria as tabelas no banco de dados se não existirem
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5000)
